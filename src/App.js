3d9a77491ba2ad3eb616cc7b2af2a2664602c5bf
tim <dzhoroev1@gmail.com>
2023-02-07 14:54:18 +0900

    [add] practice

--- src/App.js
+++ src/App.js
@@ -1,32 +1,3 @@
-// import React from "react";
-
-// function App() {
-//   const [user, setUser] = React.useState([]);
-
-//   const fetchData = () => {
-//     fetch('https://randomuser.me/api/?results=1')
-//       .then(resoponse => resoponse.json())
-//       .then(data => setUser(data));
-//   };
-
-//   React.useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   // console.log(user.res);
-
-//   return Object.keys(user).length > 0 ? (
-//     <div style={{padding: "40px"}}>
-//       <h1>Customer data</h1>
-//       <h2>Name: {user.results[0].name.first}</h2>
-//       <img src={user.results[0].picture.large} alt="" />
-//     </div>
-//   ) : (
-//     <h1>Data pending...</h1>
-//   );
-// }
-
-// export default App;
 import { useState } from "react";
 import useConsoleLog from "./useConsoleLog";
 
